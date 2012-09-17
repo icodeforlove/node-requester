54c8c1e250537a4dd8b57c0f694c664451a8c40f
Chad Scira <chadvscira@gmail.com>
2012-09-17 16:16:02 -0700

    added some more examples and removed null for query

--- examples/requests.js
+++ examples/requests.js
@@ -4,9 +4,21 @@ var	Request = require('../lib/request');
 var request = new Request();
 
 request.get('http://www.google.com', function (data) {
-	console.log(data);
+	//console.log(data);
 });
 
 request.post('http://www.google.com', function (data) {
-	console.log(data);
+	//console.log(data);
+});
+
+request.get('http://chadscira.com', {data: {foo: 'bar'}, headers: {'user-agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'}}, function (data) {
+	//console.log(data);
+});
+
+request.get('http://chadscira.com', {data: {foo: 'bar'}, headers: {'user-agent': ''}}, function (data) {
+	//console.log(data);
+});
+
+request.get('http://tumblruptime.apigee.com/json', {dataType: 'JSON'}, function (data) {
+	//console.log(data);
 });
